8b2bd53a501e2c724063ef062bf3bd93ea51663e
KwonMoose <scrt4364@gmail.com>
2022-10-26 23:07:07 +0900

    Test 환경 추가

--- Application/Program.cs
+++ Application/Program.cs
@@ -23,7 +23,7 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.EnvironmentName == "Test")
 {
     app.UseSwagger();
     app.UseSwaggerUI();
